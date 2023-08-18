bac45ea415a9d0b3982cebc033602fbc6289f742
Harpreet Pannu <er.harpreetpannu@live.com>
2023-08-17 23:24:43 -0700

    Added user controller class

---
+++ Template.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Template.WebAPI.Data;
+
+namespace Template.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<Users> _userManager;
+
+        public UsersController(UserManager<Users> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        //[HttpPost("RegisterUser")]
+        //public IActionResult RegisterUser()
+        //{
+
+        //}
+    }
+}
